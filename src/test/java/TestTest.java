cd312fe4699cda2b772488abe9e57ad9c24100a7
Sebastian Neves <sneves@impulso.edu.uy>
2026-06-16 09:04:48 -0300

    first commit

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestTest {
+    @Test
+    public void TestTrue(){
+        assertTrue(true);
+    }
+}
