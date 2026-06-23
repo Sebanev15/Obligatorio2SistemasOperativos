4a42553e58c5a3774440e75eb26d38739288c776
Sebanev15 <sneves@impulso.edu.uy>
2026-06-23 18:32:30 -0300

    add a metric to see total wait of RolCliente DOCENTE and ESTUDIANTE

--- src/main/java/org/example/GestionPedidos.java
+++ src/main/java/org/example/GestionPedidos.java
@@ -53,6 +53,7 @@ public class GestionPedidos {
         int sumaTiempoEspera=0;
         Set <Long> pedidosInaceptables = new HashSet<>();
         Map <Producto,Integer> ventas= new HashMap<>();
+        Map <RolCliente,Integer> tiempoPorRol = new HashMap();
         System.out.println("===== CAJA HOY =====");
         for (Pedido p:pedidosCompletados){
             profit+=p.getProducto().getPrecio();
@@ -67,6 +68,11 @@ public class GestionPedidos {
             if (p.getTiempoEspera()>100){
                 pedidosInaceptables.add(p.getId());
             }
+            if(p.getCliente().getRol().equals(RolCliente.ESTUDIANTE)){
+                tiempoPorRol.put(RolCliente.ESTUDIANTE, tiempoPorRol.getOrDefault(RolCliente.ESTUDIANTE, 0) + p.getTiempoEspera());
+            } else if(p.getCliente().getRol().equals(RolCliente.DOCENTE)){
+                tiempoPorRol.put(RolCliente.DOCENTE, tiempoPorRol.getOrDefault(RolCliente.DOCENTE, 0) + p.getTiempoEspera());
+            }
         }
 
         pedidosCompletados.clear();
@@ -78,6 +84,8 @@ public class GestionPedidos {
         System.out.println("Tiempo promedio de espera: "+(double)sumaTiempoEspera/cantidadPedidos);
         double porcentajeUsoCafetera = ((double) ventas.get(Producto.CAFE) / cantidadPedidos) * 100;
         System.out.println("Porcentaje de uso de la cafetera: " + porcentajeUsoCafetera + "%");
+        System.out.println("Tiempo de espera total de estudiantes: " + tiempoPorRol.get(RolCliente.ESTUDIANTE));
+        System.out.println("Tiempo de espera total de docentes: " + tiempoPorRol.get(RolCliente.DOCENTE));
         if (pedidosInaceptables.isEmpty()){
             System.out.println("No hubo pedidos con exceso de espera");
         } else {
